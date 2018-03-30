251a36bf02d8860c7e74b7057b3b87bdb506be5f
Josh Long <starbuxman@gmail.com>
2018-03-30 16:25:34 -0700

    greetings

---
+++ src/main/kotlin/com/example/reactiveweb/Greeting.java
@@ -0,0 +1,18 @@
+package com.example.reactiveweb;
+
+/**
+	* @author <a href="mailto:josh@joshlong.com">Josh Long</a>
+	*/
+public class Greeting {
+ 
+		private final String text;
+
+		public String getText() {
+				return text;
+		}
+
+		public Greeting(String t) {
+				this.text = t;
+		}
+}
+
